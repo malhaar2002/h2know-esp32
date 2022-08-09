3b89a9eeb4986e29d388b0209d8ef38ef5718771
malhaar2002 <aroramalhaar@gmail.com>
2022-08-09 23:21:08 +0530

    Integrated flow rate sensor with RTDB

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,33 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include <Firebase_ESP_Client.h>
 
+// Provide the token generation process info.
+#include "addons/TokenHelper.h"
+// Provide the RTDB payload printing info and other helper functions.
+#include "addons/RTDBHelper.h"
+
+// Insert your network credentials
+#define WIFI_SSID "Redmi Note 10 Pro"
+#define WIFI_PASSWORD "malconnect"
+
+// Insert Firebase project API Key
+#define API_KEY "AIzaSyB5UILE695vTOFR7BWsZWC8Zla2tvb5GwI"
+
+// Insert RTDB URLefine the RTDB URL */
+#define DATABASE_URL "https://h2know-40972-default-rtdb.asia-southeast1.firebasedatabase.app/"
+
+// Define Firebase Data object
+FirebaseData fbdo;
+
+// Define flow rate sensor location
 #define SENSOR 4
+
+FirebaseAuth auth;
+FirebaseConfig config;
+
+unsigned long sendDataPrevMillis = 0;
+bool signupOK = false;
 
 long currentMillis = 0;
 long previousMillis = 0;
@@ -22,6 +49,7 @@ void setup()
 {
   Serial.begin(9600);
 
+  // Sensor setup
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(SENSOR, INPUT_PULLUP);
 
@@ -32,6 +60,42 @@ void setup()
   previousMillis = 0;
 
   attachInterrupt(digitalPinToInterrupt(SENSOR), pulseCounter, FALLING);
+
+  // Firebase setup
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting to Wi-Fi");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+      Serial.print(".");
+      delay(300);
+  }
+  Serial.println();
+  Serial.print("Connected with IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.println();
+
+  /* Assign the api key (required) */
+  config.api_key = API_KEY;
+
+  /* Assign the RTDB URL (required) */
+  config.database_url = DATABASE_URL;
+
+  /* Sign up */
+  if (Firebase.signUp(&config, &auth, "", ""))
+  {
+      Serial.println("ok");
+      signupOK = true;
+  }
+  else
+  {
+      Serial.printf("%s\n", config.signer.signupError.message.c_str());
+  }
+
+  /* Assign the callback function for the long running token generation task */
+  config.token_status_callback = tokenStatusCallback; // see addons/TokenHelper.h
+
+  Firebase.begin(&config, &auth);
+  Firebase.reconnectWiFi(true);
 }
 
 void loop()
@@ -60,9 +124,9 @@ void loop()
     
     // Print the flow rate for this second in litres / minute
     Serial.print("Flow rate: ");
-    Serial.print(int(flowRate));  // Print the integer part of the variable
+    Serial.print(int(flowRate));
     Serial.print("L/min");
-    Serial.print("\t");       // Print tab space
+    Serial.print("\t");
 
     // Print the cumulative total of litres flowed since starting
     Serial.print("Output Liquid Quantity: ");
@@ -70,5 +134,35 @@ void loop()
     Serial.print("mL / ");
     Serial.print(totalMilliLitres / 1000);
     Serial.println("L");
+
+    if (Firebase.ready() && signupOK && (millis() - sendDataPrevMillis > 15000 || sendDataPrevMillis == 0))
+    {
+        sendDataPrevMillis = millis();
+        // Write email address on path test/email
+        if (Firebase.RTDB.setString(&fbdo, "test/email", "malhaar.arora@plaksha.edu.in"))
+        {
+            Serial.println("PASSED");
+            Serial.println("PATH: " + fbdo.dataPath());
+            Serial.println("TYPE: " + fbdo.dataType());
+        }
+        else
+        {
+            Serial.println("FAILED");
+            Serial.println("REASON: " + fbdo.errorReason());
+        }
+
+        // Write volume used on path test/volume
+        if (Firebase.RTDB.setFloat(&fbdo, "test/volume", totalMilliLitres/1000.0))
+        {
+            Serial.println("PASSED");
+            Serial.println("PATH: " + fbdo.dataPath());
+            Serial.println("TYPE: " + fbdo.dataType());
+        }
+        else
+        {
+            Serial.println("FAILED");
+            Serial.println("REASON: " + fbdo.errorReason());
+        }
+    }
   }
 }
